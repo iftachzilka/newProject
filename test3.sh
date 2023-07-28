c314b31552b75b93ff7a246600d7f2e08326be9e
iftachzilka <iftachzilka@.hghjg.com>
2023-07-28 16:17:46 +0300

    ddas

--- test3.sh
+++ test3.sh
@@ -11,35 +11,12 @@ echo
 # set content to a YAML file
 echo "123" >> file.yml
 
-echo
-# Pause the script until the user presses Enter
-read -n 1 -s -p "--- Now we are adding commit and push to remote repo ---"
-echo
 
 # push to repo
 git add file.yml
 git commit -m "Added text to YAML file"
 git push origin testtt
 
-echo
-# Pause the script until the user presses Enter
-read -n 1 -s -p "--- Now we open a pr and the remote repo --- "
-echo
-
-# Get the title and body from the user
-#read -p "Title: " title
-#read -p "Body: " body
-
-# Open a pull request
-
-#apt install gh
-
-#prr=`gh pr create  -t "title" -b "body"`
-echo $prr
-ttt="${prr##*/}"
-echo $ttt
-#echo $ttt > 1234.txt
-
 
 
 
@@ -48,7 +25,3 @@ echo
 echo
 
 
-
-#cd ..
-#echo $ttt > 1234.txt
-#rm -r ./newProject
