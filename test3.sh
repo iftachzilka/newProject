e35912ea7b9a327222db31170c7d9b8d5f51d054
iftachzilka <iftachzilka@.hghjg.com>
2023-07-28 15:08:38 +0300

    new

--- test3.sh
+++ test3.sh
@@ -3,44 +3,24 @@
 
 
 
-YELLOW=`tput setaf 3`
-WHITE=`tput setaf 7 bold`
-
-echo "${YELLOW}${branch}"
-echo "${WHITE}"
-
-#echo $branch
-#echo -e "\033[5;33m${branch}"
-
-#shell variables
-#BR_NAME=$branch
 
 echo
-echo "${YELLOW}"
-# Pause the script until the user presses Enter
-#read -n 1 -s -p "--- Now we are cloning the git repo ---"
-#echo "${WHITE}"
-#echo
 
 
 # Check if the repository exists.
 if [ -d "newProject" ]; then
   # The repository exists, try to pull.
   echo
-  echo "${YELLOW}"
   # Pause the script until the user presses Enter
   read -n 1 -s -p "--- Now we are pulling the git repo ---"
-  echo "${WHITE}"
   echo
   cd newProjectt
   git pull origin main
 else
   # The repository does not exist, clone it.
   echo
-  echo "${YELLOW}"
   # Pause the script until the user presses Enter
   read -n 1 -s -p "--- Now we are cloning the git repo ---"
-  echo "${WHITE}"
   echo
   gh repo clone iftachzilka/newProject
   cd newProject
@@ -57,48 +37,38 @@ fi
 #split_argument=$(awk -F/ '{print $2}' <<< "$repo")
 
 echo
-echo "${YELLOW}"
 # Print the new argument.
 echo $split_argument
-echo "${WHITE}"
 echo
 
 
 #echo
-#echo "${YELLOW}"
 # Pause the script until the user presses Enter
 #read -n 1 -s -p "--- Now go into the git folder ---"
-#echo "${WHITE}"
 #echo
 
 # Change to the cloned directory
 #cd $split_argument
 
 echo
-echo "${YELLOW}"
 # Pause the script until the user presses Enter
 read -n 1 -s -p "--- Now creating a branch ---"
-echo "${WHITE}"
 echo
 
 #create new branch
 git checkout -b testt1
 
 echo
-echo "${YELLOW}"
 # Pause the script until the user presses Enter
 read -n 1 -s -p "--- Now we add a text into a  yaml file ---"
-echo "${WHITE}"
 echo
 
 # set content to a YAML file
 echo "123" >> file.yml
 
 echo
-echo "${YELLOW}"
 # Pause the script until the user presses Enter
 read -n 1 -s -p "--- Now we are adding commit and push to remote repo ---"
-echo "${WHITE}"
 echo
 
 # push to repo
@@ -107,10 +77,8 @@ git commit -m "Added text to YAML file"
 git push origin testt1
 
 echo
-echo "${YELLOW}"
 # Pause the script until the user presses Enter
 read -n 1 -s -p "--- Now we open a pr and the remote repo --- "
-echo "${WHITE}"
 echo
 
 # Get the title and body from the user
@@ -120,9 +88,7 @@ echo
 # Open a pull request
 
 prr=`gh pr create  -t "title" -b "body"`
-echo "${YELLOW}"
 echo $prr
-echo "${WHITE}"
 ttt="${prr##*/}"
 echo $ttt
 #echo $ttt > 1234.txt
@@ -130,13 +96,10 @@ echo $ttt
 
 
 prr1=`gh pr view`
-echo "${YELLOW}"
 echo $prr1
-echo "${WHITE}"
 # Get the argument.
 #argument=$1
 
-echo "${YELLOW}"
 # Check if the argument contains the string "hello".
 # Check if the first argument contains some string from the second argument.
 if [[ $prr1 =~ "$ttt" ]]; then
