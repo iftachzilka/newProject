bce83e4485383e106aa654b53202fbb7b62e2d8c
iftachzilka <iftachzilka@.hghjg.com>
2023-07-28 16:21:19 +0300

    ddas

--- test3.sh
+++ test3.sh
@@ -11,7 +11,8 @@ echo
 # set content to a YAML file
 echo "123" >> file.yml
 
-
+echo $(git rev-parse --abbrev-ref HEAD)
+echo $(git branch --show-current)
 # push to repo
 git add file.yml
 git commit -m "Added text to YAML file"
